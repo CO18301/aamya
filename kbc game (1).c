20fdbf3248cae917c2bf2b9a6db81f2413720280
CO18301 <42859342+CO18301@users.noreply.github.com>
2018-09-03 20:57:51 +0530

    Add files via upload

---
+++ kbc game (1).c
@@ -0,0 +1,174 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char A;
+    system("cls");
+    printf("\nYOU ARE PLAYING KON BANEGA CROREPATI\n");
+
+    printf("\nLets have our first question\n");
+    printf("\n1. Who is Winner of Football worldcup 2018?\n");
+    printf("a) 1Germany\t\t\tb)Ukraine \nc)Spain\t\t\td)France\n");
+    scanf("%c", &A);
+    if(A=='d')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 1000 Rupees\n\n");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+
+    char B;
+    printf("\nLets have our Second question\n");
+    printf("\n1. Who is PM of India ?\n");
+    printf("a)Narendar Modi\t\t\tb)Rahul Gandhi \nc)Advani ji\t\t\td)Arvind Kejriwal\n");
+    scanf("%c", &B);
+    scanf("%c", &B);
+    if(B=='a')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 2000 Rupees\n\n");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+
+    }
+    char C;
+    printf("\nLets have our Third question\n");
+    printf("\n1. What is the last fastest record of 100m men race?\n");
+    printf("a)10 sec 1\t\t\tb)7 sec \nc)9.58 sec\t\t\td)9.23 sec\n");
+    scanf("%c", &C);
+    scanf("%c", &C);
+    if(C=='c')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 3000 Rupees\n\n");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+    char D;
+    printf("\nLets have our Fourth question\n");
+    printf("\n1. What is multiplication of 10 and 15 ?\n");
+    printf("a) 100\t\t\tb) 1500 \nc) 15\t\t\td) 150\n");
+    scanf("%c", &D);
+    scanf("%c", &D);
+    if(D=='d')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 4000 Rupees\n\n");
+    }
+
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+    char E;
+    printf("\nLets have our Fifth question\n");
+    printf("\n1. What is sum of 2 and 3 ?\n");
+    printf("a) 1\t\t\tb) 7 \nc) 5\t\t\td) 9\n");
+    scanf("%c", &E);
+    scanf("%c", &E);
+    if(E=='c')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 5000 Rupees\n\n");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+    char F;
+    printf("\nLets have our Sixth question\n");
+    printf("\n1. When Hitler became the chancellor of Germany?\n");
+    printf("a) 1972\t\t\tb)1933 \nc)1974\t\t\td)1979\n");
+    scanf("%c", &F);
+    scanf("%c", &F);
+    if(F=='b')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 6000 Rupees\n\n");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+    char G;
+    printf("\nLets have our Seventh question\n");
+    printf("\n1. What is the chemical Formula of water?\n");
+    printf("a) H2SO4\t\t\tb)CO2 \nc)HNO3\t\t\td)H2O\n");
+    scanf("%c", &G);
+    scanf("%c", &G);
+    if(G=='d')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 7000 Rupees\n\n");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+    char H;
+    printf("\nLets have our Eighth question\n");
+    printf("\n1. what is the chemical formula for Nitric Acid.?\n");
+    printf("a) HNO3\t\t\tb)H2SO4 \nc)HCL\t\t\td)None of these\n");
+    scanf("%c", &H);
+    scanf("%c", &H);
+    if(H=='a')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 8000 Rupees\n\n");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+    char I;
+    printf("\nLets have our Ninth question\n");
+    printf("\n1. which of the following is Carnivore animal?\n");
+    printf("a) Deer\t\t\tb)Cow \nc)Buffalow\t\t\td)Lion\n");
+    scanf("%c", &I);
+    scanf("%c", &I);
+    if(I=='d')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 9000 Rupees\n\n");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+    char J;
+    printf("\nLets have our Final question\n");
+    printf("\n1. who is the Father of Lord Rama in Ramayan?\n");
+    printf("a) Bharat\t\t\tb)Dashrath \nc)Laxman\t\t\td)None of these\n");
+    scanf("%c", &J);
+    scanf("%c", &J);
+    if(J=='b')
+    {
+        printf("\ncorrect answer..well played..!!\n");
+        printf("\nYOU HAVE WON 10000 Rupees\n\n");
+        printf("\n\n\n\nYOU WON THE GAME\n\n\n\n ");
+    }
+    else
+    {
+        printf("\nIncorrect Answer..Sorry you loose the Game..!!\n");
+        exit(0);
+    }
+
+    return 0;
+
+}
